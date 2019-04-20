0694add1001d5ef22345c0551331cb1f211cfd37
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2019-04-20 13:38:04 -0400

    Simulate app with local data.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import cachedResponse from './freeline_project_description';
+import response0 from './response-0';
+import response1 from './response-1';
+import response2 from './response-2';
 
 import './App.css';
+import './loader.css';
 
 const getUrl = id => `https://media2.giphy.com/media/${id}/200w.gif`;
+
+const mockResponses = [response0, response1, response2];
+
+const testing = false;
 
 class App extends Component {
   state = {
     data: [],
     gifs: [],
     offset: 0,
+    page: 0,
     loading: null,
   };
 
   loadData = offset => {
-    fetch(`http://localhost:8000/api/gifs?offset=${offset}`)
+    const apiUrl = process.env.REACT_APP_API_URL;
+    return fetch(`${apiUrl}?offset=${offset}`)
       .then(response => response.json())
       .then(gifObject => this.onSetData(gifObject))
       .catch(error => console.log(error));
   };
 
-  loadLocalData = () => {
-    const { data } = cachedResponse;
-    this.setState({ data });
+  mockLoadData = index => {
+    const { page } = this.state;
+    const computedIndex = index || page;
+    const mockResponse = mockResponses[computedIndex];
+
+    return Promise.resolve(mockResponse)
+      .then(gifObject => this.onSetData(gifObject))
+      .catch(error => console.log(error));
   };
 
   onSetData = gifObject => {
@@ -40,24 +54,41 @@ class App extends Component {
     });
   };
 
+  mockfetchNextPage = () => {
+    const { page } = this.state;
+    if (page >= 3) {
+      return;
+    }
+
+    this.mockLoadData(page);
+
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
   fetchNextPage = () => {
+    if (testing) {
+      this.mockfetchNextPage();
+      return;
+    }
+
     const { offset } = this.state;
     this.loadData(offset);
-    const newOffset = offset + 25;
-    this.setState({
-      offset: newOffset,
-    });
+    this.setState(prevState => ({
+      offset: prevState.offset + 25,
+    }));
   };
 
   onScroll = () => {
     this.handleLazyLoading();
+    const { innerHeight, scrollY } = window;
+    const { offsetHeight } = document.body;
 
-    if (
-      window.innerHeight + window.scrollY >=
-      document.body.offsetHeight - 500
-    ) {
+    const reachedBottom = innerHeight + scrollY >= offsetHeight;
+    if (reachedBottom) {
       this.setState({ loading: true });
-      // this.fetchNextPage();
+      this.fetchNextPage();
     }
   };
 
@@ -70,14 +101,18 @@ class App extends Component {
       }
 
       lazyloadThrottleTimeout = setTimeout(() => {
-        const scrollTop = window.pageYOffset;
+        const { pageYOffset: scrollTop, innerHeight } = window;
         const lazyloadImages = document.querySelectorAll('.lazy');
+
         lazyloadImages.forEach(img => {
-          if (img.offsetTop < window.innerHeight + scrollTop) {
+          const imageInViewport = img.offsetTop < innerHeight + scrollTop;
+          if (imageInViewport) {
             img.src = img.dataset.src;
+            img.style.height = '100%';
             img.classList.remove('lazy');
           }
         });
+
         if (lazyloadImages.length === 0) {
           document.removeEventListener('scroll', lazyload);
           window.removeEventListener('resize', lazyload);
@@ -92,7 +127,6 @@ class App extends Component {
   };
 
   componentDidMount() {
-    // this.loadLocalData();
     this.fetchNextPage();
     window.addEventListener('scroll', this.onScroll, false);
   }
@@ -107,28 +141,30 @@ class App extends Component {
       <div className="App">
         <div className="image-grid">
           {gifs.map((gif, index) =>
-            index < 8 ? (
-              <img
-                key={gif.id}
-                className="image-item"
-                alt={gif.title}
-                width={gif.width}
-                height={gif.height}
-                src={getUrl(gif.id)}
-              />
+            index < 15 ? (
+              <div className="image-item" key={gif.id}>
+                <img
+                  className="image"
+                  alt={gif.title}
+                  width={gif.width}
+                  height={gif.height}
+                  src={getUrl(gif.id)}
+                />
+              </div>
             ) : (
-              <img
-                key={gif.id}
-                className="image-item lazy"
-                alt={gif.title}
-                width={gif.width}
-                height={gif.height}
-                data-src={getUrl(gif.id)}
-              />
+              <div className="image-item lazy" key={gif.id}>
+                <img
+                  className="image lazy"
+                  alt={gif.title}
+                  width={gif.width}
+                  height={gif.height}
+                  data-src={getUrl(gif.id)}
+                />
+              </div>
             )
           )}
         </div>
-        {loading && <h2>Loading...</h2>}
+        {loading && <div className="loader" />}
       </div>
     );
   }
